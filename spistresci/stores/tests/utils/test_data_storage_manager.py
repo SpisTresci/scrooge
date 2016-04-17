ca60cf3aa89e116a2af4aa689ad6dbe849c55fde
Krzysztof Szumny <noisy.pl@gmail.com>
2016-04-17 21:51:24 +0200

    DataStorageManager.last_revision_number()

--- spistresci/stores/tests/utils/test_data_storage_manager.py
+++ spistresci/stores/tests/utils/test_data_storage_manager.py
@@ -1,3 +1,4 @@
+import traceback
 from tempfile import TemporaryDirectory
 from test_plus.test import TestCase
 
@@ -14,10 +15,16 @@ class TestDataStorageManager(TestCase):
     def tearDown(self):
         self.temp_dir.cleanup()
 
-    def test_data_are_saved(self):
+    def _func_name(self):
+        """
+        To avoid using the same store_name for different tests, you can
+        use testcase function name as store_name
+        """
+        return traceback.extract_stack(None, 2)[0][2]
 
+    def test_data_are_saved(self):
         with override_settings(ST_STORES_DATA_DIR=self.temp_dir.name):
-            ds_manager = DataStorageManager('the store name')
+            ds_manager = DataStorageManager(self._func_name())
             with ds_manager.save('file.xml') as buffer:
                 buffer.write(b'data part 1')
                 buffer.write(b'data part 2')
@@ -25,3 +32,57 @@ class TestDataStorageManager(TestCase):
             content = ds_manager.get('file.xml')
 
         self.assertEqual(content, 'data part 1data part 2')
+
+    def test_last_revision_number_is_0_at_the_beginning(self):
+        with override_settings(ST_STORES_DATA_DIR=self.temp_dir.name):
+            ds_manager = DataStorageManager(self._func_name())
+            self.assertEqual(ds_manager.last_revision_number(), 0)
+
+    def test_last_revision_number_increase_after_each_save(self):
+        with override_settings(ST_STORES_DATA_DIR=self.temp_dir.name):
+            ds_manager = DataStorageManager(self._func_name())
+            self.assertEqual(ds_manager.last_revision_number(), 0)
+
+            with ds_manager.save('file.xml') as buffer:
+                buffer.write(b'data part 1')
+
+            self.assertEqual(ds_manager.last_revision_number(), 1)
+
+            with ds_manager.save('file.xml') as buffer:
+                buffer.write(b'data part 1')
+                buffer.write(b'data part 2')
+
+            self.assertEqual(ds_manager.last_revision_number(), 2)
+
+    def test_last_revision_number_do_not_depend_on_filename(self):
+        with override_settings(ST_STORES_DATA_DIR=self.temp_dir.name):
+            ds_manager = DataStorageManager(self._func_name())
+            self.assertEqual(ds_manager.last_revision_number(), 0)
+
+            with ds_manager.save('file1.xml') as buffer:
+                buffer.write(b'data part 1')
+
+            self.assertEqual(ds_manager.last_revision_number(), 1)
+
+            with ds_manager.save('file2.xml') as buffer:
+                buffer.write(b'data part 1')
+
+            self.assertEqual(ds_manager.last_revision_number(), 2)
+
+    def test_last_revision_number_do_not_depend_on_store_name(self):
+        with override_settings(ST_STORES_DATA_DIR=self.temp_dir.name):
+            ds_manager = DataStorageManager(self._func_name() + '1')
+            self.assertEqual(ds_manager.last_revision_number(), 0)
+
+            with ds_manager.save('file1.xml') as buffer:
+                buffer.write(b'data part 1')
+
+            self.assertEqual(ds_manager.last_revision_number(), 1)
+
+            ds_manager = DataStorageManager(self._func_name() + '2')
+            self.assertEqual(ds_manager.last_revision_number(), 0)
+
+            with ds_manager.save('file1.xml') as buffer:
+                buffer.write(b'data part 1')
+
+            self.assertEqual(ds_manager.last_revision_number(), 1)
--- spistresci/stores/utils/datastoragemanager.py
+++ spistresci/stores/utils/datastoragemanager.py
@@ -7,6 +7,7 @@ from django.conf import settings
 
 
 class DataStorageManager:
+    __revision_tag_name = 'rev-{}'
 
     def __init__(self, store_name):
         self.store_name = store_name
@@ -23,27 +24,41 @@ class DataStorageManager:
     @contextmanager
     def save(self, filename):
         self.__asert_is_clean()
-        filepath = os.path.join(self.store_storage_dir, filename)
+        file_path = os.path.join(self.store_storage_dir, filename)
 
-        file = open(filepath, 'wb')
+        file = open(file_path, 'wb')
         yield file
 
         file.close()
-        self.repo.index.add([filepath])
+        self.repo.index.add([file_path])
 
         commit_date = datetime.now()
         commit_datetime_str = commit_date.strftime("%Y-%m-%d %H:%M:%S")
         commit_msg = "Store: {}\nDate: {}".format(self.store_name, commit_datetime_str)
 
         self.repo.index.commit(commit_msg)
+        self.__increment_revision()
 
     def get(self, filename):
         self.__asert_is_clean()
 
-        filepath = os.path.join(self.store_storage_dir, filename)
-        with open(filepath) as f:
+        file_path = os.path.join(self.store_storage_dir, filename)
+        with open(file_path) as f:
             return f.read()
 
     def __asert_is_clean(self):
         assert not self.repo.is_dirty(), "Repository '{}' is dirty. " \
             "Has to be cleaned up before further work.".format(self.store_storage_dir)
+
+    def __increment_revision(self):
+        rev = self.last_revision_number()
+        self.repo.create_tag(self.__revision_tag_name.format(rev + 1))
+
+    def last_revision_number(self):
+        revisions = [
+            int(tag.name.replace(self.__revision_tag_name.format(''), ''))
+            for tag in self.repo.tags
+            if tag.name.startswith(self.__revision_tag_name.format(''))
+        ]
+
+        return 0 if not revisions else max(revisions)
