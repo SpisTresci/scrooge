SpisTresci/scrooge

commit 805f2d05cbd671c25da040c1d712c067699fdd20

Initial support for Woblink + support for big XML files

spistresci/stores/datasource/generic.py
@@ -1,6 +1,6 @@
 import logging
 import re
-from lxml import etree
+from lxml import etree, objectify
 from urllib.request import urlopen, Request
 
 from spistresci.stores.models import Store
@@ -142,7 +142,8 @@ class XmlDataSource(DataSource):
     depth = 0
 
     def _get_product_list(self, file_content):
-        root = etree.fromstring(str.encode(file_content))
+        parser = objectify.makeparser(huge_tree=True)
+        root = etree.fromstring(str.encode(file_content), parser)
         return list(self._we_have_to_go_deeper(root, self.depth))
 
     def _we_have_to_go_deeper(self, root, depth):

spistresci/stores/datasource/specific/__init__.py
@@ -1,1 +1,2 @@
 from spistresci.stores.datasource.specific.publio import Publio
+from spistresci.stores.datasource.specific.woblink import Woblink

spistresci/stores/datasource/specific/woblink.py
@@ -1,0 +1,19 @@
+from spistresci.stores.datasource.generic import XmlDataSource
+
+
+class Woblink(XmlDataSource):
+    depth = 1
+
+    xml_tag_dict = {
+        'external_id': ('@id', ''),
+        'price': ('@price', 0),
+        'url': ('@url', ''),
+        'availability': ('@avail', ''),
+        'category': ('./cat', ''),
+        'title': ('./name', ''),
+        'cover': ('./imgs/main/@url', ''),
+        'description': ('./desc', ''),
+        'isbns': ("./attrs/a[@name='ISBN']", ''),
+        'publisher': ("./attrs/a[@name='Wydawnictwo']", ''),
+        'formats': ("./attrs/a[@name='Format']", ''),
+    }

commit d3dfbecb45e1649a09200eaf8c7e7ac4203f6a63

Fix for using wrong parser. Bug introduced in 805f2d05cbd671c25da040c1d712c067699fdd20

spistresci/stores/datasource/generic.py
@@ -1,6 +1,6 @@
 import logging
 import re
-from lxml import etree, objectify
+from lxml import etree
 from urllib.request import urlopen, Request
 
 from spistresci.stores.models import Store
@@ -142,7 +142,7 @@ class XmlDataSource(DataSource):
     depth = 0
 
     def _get_product_list(self, file_content):
-        parser = objectify.makeparser(huge_tree=True)
+        parser = etree.XMLParser(huge_tree=True)
         root = etree.fromstring(str.encode(file_content), parser)
         return list(self._we_have_to_go_deeper(root, self.depth))
 